88aa9b344fe5aa066dc0adf962273e0b5053c460
Kent Davy <kdavy@mdsol.com>
2014-08-17 16:10:41 -0700

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains a functions for creating and calculating the 
+## inverse of special "matrix" objects which are capable of caching 
+## their inverses.  The inverse of a matrix can be quite expensive to 
+## calculate for large matrixes and caching the results can speed things
+## up if the inverse of the same matrix is required multiple times.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special matrix which is capable of caching its inverse.
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
     set <- function(y) {
@@ -25,12 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
          get_inverse = get_inverse)
 }
 
-## Write a short comment describing this function
+## `cacheSolve`: This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
     inv <- x$get_inverse()
     if (is.null(inv)) {
-        print("Calculating inverse")
         data <- x$get()
         inv <- solve(data)
         x$set_inverse(inv)
@@ -38,60 +42,3 @@ cacheSolve <- function(x, ...) {
     inv
 }
 
-makeCached <- function(name,funcs) {
-    cache <- replicate(length(funcs),NULL)
-    names(cache) <- names(funcs)
-    make_fun <- function(x) {
-        force(x)
-            
-        set <- function(fname,y) {
-            x <<- y
-            cache[fname] <<- NULL
-        }
-        get <- function() {
-            x
-        }
-        set_cached <- function(fname,cv) {
-            cache[[fname]] <<- cv
-        }
-        get_cached <- function(fname) {
-            cache[[fname]]
-        }
-        
-        list(set = set,
-             get = get,
-             set_cached = set_cached,
-             get_cached = get_cached)
-    }
-    assign(name,make_fun,envir=parent.env(environment()))
-    for (fname in names(funcs)) {
-        
-        z <- function () {
-            funcname <- fname
-            f <- function(x) {
-                print(cache)
-                cv <- cache[[funcname]]
-                if (is.null(cv)) {
-                    print(paste("Calculating ", funcname))
-                    data <- x$get()
-                    cv <- funcs[[funcname]](data)
-                    x$set_cached(funcname,cv)
-                } else {
-                    print(paste("Getting cached value ", funcname))
-                }
-                cv
-            }
-            assign(fname,f,pos=parent.env(parent.env(environment())))
-        }
-        z()
-        
-       
-        #assign(fname,f,pos=parent.env(environment()))
-        #assign("funcname",paste(fname))
-        #print(environment(f))
-        #print(environment())
-    }
-}
-
-makeCached("makeCachedMatrix2",list(cacheSolve2=function(x) {solve(x)},
-                                    cacheSum2=function(x) {mean(x)}))
